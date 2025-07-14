004b2f6e8ec280986cec6a9df02d18c728f8064e
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-14 12:12:37 +0530

    https and url

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, res)=>{
+    if (req.url === "/favicon.ico" && req.url === "/firebase-messaging-sw.js")
+      return res.end();
     const log = `${Date.now()}: ${req.url} New request found \n`
      fs.appendFile("./log.txt",log,(err,data)=>{
         switch(req.url){
