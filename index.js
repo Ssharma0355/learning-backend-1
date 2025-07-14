66da3a539ce29a714d322d7146724b399785e410
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-14 10:40:26 +0530

    servers-and url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, res)=>{
-    const log = `${Date.now()}:New request found \n`
+    const log = `${Date.now()}: ${req.url} New request found \n`
      fs.appendFile("./log.txt",log,(err,data)=>{
     res.end("Hi from the server");
      })
